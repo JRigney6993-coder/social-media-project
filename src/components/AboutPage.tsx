fea07cd796171c8cc2f52497d1b4b44c26eb92ab
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2023-12-12 10:34:21 -0800

    a

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,16 +1,15 @@
 import { siteConfig } from "@/config/site";
 import { buttonVariants } from "@/components/ui/button";
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Social media made perfect <br className="hidden sm:inline" />
-          With Grape chat.
+          About us
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Grape chat is a social platform which utilizes threads to give users the best experience. Threads are a great way to communicate and share your opinion about topics. Users are able to create, like, and comment to threads.
+          This was a project made in 4 days by Jacob Rigney and Andrew Murphy. We used, React, Typescript, Vite, Next.js, and mongodb to create this social platform.
         </p>
       </div>
       <div className="flex gap-4">
