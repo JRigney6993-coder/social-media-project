b43cb4bf0d171b8c61e5b31f1bc51d1f98052402
Dearborn42 <amurf26@outlook.com>
2023-12-18 00:57:26 -0700

    Made profile page for users

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -21,7 +21,27 @@ const Profile = () => {
     getProfile();
   }, [])
   return (
-    <div>{}</div>
+    // <div>{}</div>
+    <div className="flex items-center justify-center flex-col min-h-screen border-gray-900/10  text-white font-montserrat">
+  <div className="card-container mt-16 ring-gray-300 rounded-lg shadow-md text-gray-400 pt-8 relative w-80 md:w-96 text-center">
+    <img className="round border-solid border-cyan-500 rounded-full p-7" src={profile['profile_pic']} alt="user" />
+    <div className="buttons mt-5 border-b border-gray-900/10">
+      <h1>{profile["user_name"]}</h1>
+    </div>
+    <div className="skills ring-gray-300 text-left px-4 py-5 mt-8">
+      <h6 className="text-white">Skills</h6>
+      <ul className="list-none m-0 p-0">
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">UI / UX</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">Front End Development</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">HTML</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">CSS</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">JavaScript</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">React</li>
+        <li className="border-solid border-purple-900 border rounded-md inline-block text-xs px-3 py-2 mr-2 mb-2">Node</li>
+      </ul>
+    </div>
+  </div>
+</div>
   )
 }
 
