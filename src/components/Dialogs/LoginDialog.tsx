c045bb5d9b2be60a28cdff16884e5a24929af3b9
Dearborn42 <amurf26@outlook.com>
2023-12-18 22:34:03 -0700

    Working on integration

--- src/components/Dialogs/LoginDialog.tsx
+++ src/components/Dialogs/LoginDialog.tsx
@@ -7,38 +7,80 @@ import {
 import {Label} from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react';
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        credentials: 'include',
+        body: JSON.stringify(formData),
+      });
+
+      var result = await response.json();
+      if (result.success){
+        localStorage.setItem('userToken', result);
+      }
+
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+  };
+
   return (
     <>
-    <form action="">
-    <DialogHeader>
-      <DialogTitle>Login</DialogTitle>
-      <DialogDescription>
-        Forgot your password? <a className="text-purple" href="/">Click here!</a>
-      </DialogDescription>
-    </DialogHeader><div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="email" className="text-right">
-            Email
-          </Label>
-          <Input
-            id="email"
-            className="col-span-3" />
+    <form onSubmit={handleSubmit}>
+     <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Forgot your password? <a className="text-purple" href="/">Click here!</a>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              className="col-span-3"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="password" className="text-right">
+              Password
+            </Label>
+            <Input
+              id="password"
+              name="password"
+              className="col-span-3"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="password" className="text-right">
-            Password
-          </Label>
-          <Input
-            id="password"
-            className="col-span-3"
-            type="password" />
-        </div>
-      </div><DialogFooter>
-        <Button type="submit">Submit</Button>
-      </DialogFooter>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
       </form>
-      </>
+    </>
   )
 }
--- src/components/Dialogs/SignupDialog.tsx
+++ src/components/Dialogs/SignupDialog.tsx
@@ -6,23 +6,74 @@ import {
 } from "@/components/ui/dialog"
 import {Label} from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    name: '',
+    user_name: '',
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/users/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        credentials: 'include',
+        body: JSON.stringify(formData),
+      });
+
+      var result = await response.json();
+      if (result.success){
+        localStorage.setItem('userToken', result);
+      }
+
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+  };
   return (
-    <><DialogHeader>
+    <>
+    <form onSubmit={handleSubmit}>
+    <DialogHeader>
       <DialogTitle>Signup</DialogTitle>
       <DialogDescription>
         Remember to keep your password is secure!
       </DialogDescription>
     </DialogHeader><div className="grid gap-4 py-4">
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
+          <Label htmlFor="user_name" className="text-right">
             Username
           </Label>
           <Input
-            id="username"
-            className="col-span-3" />
+            id="user_name"
+            className="col-span-3"
+            name="user_name"
+            value={formData.user_name}
+            onChange={handleChange} 
+          />
+        </div>
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="name" className="text-right">
+            Legal Name
+          </Label>
+          <Input
+            id="name"
+            className="col-span-3" 
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            />
         </div>
         <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="email" className="text-right">
@@ -30,7 +81,11 @@ export default function Signup() {
           </Label>
           <Input
             id="email"
-            className="col-span-3" />
+            className="col-span-3" 
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="password" className="text-right">
@@ -39,11 +94,17 @@ export default function Signup() {
           <Input
             id="password"
             className="col-span-3"
-            type="password" />
+            type="password" 
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
       </div><DialogFooter>
         <Button type="submit">Continue</Button>
-      </DialogFooter></>
+      </DialogFooter>
+      </form>
+    </>
   )
 }
 
